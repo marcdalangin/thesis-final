b31beaf62dd41d3d693fc54569f112ee3d9869cd
marcdalangin0819 <marcdalangin0819@gmail.com>
2023-12-13 21:04:17 +0800

    Edited presentation.py

--- presentation.py
+++ presentation.py
@@ -112,6 +112,7 @@ while True:
     imgSmall = cv2.resize(img, (ws, hs))
     h, w, _ = imgCurrent.shape
     imgCurrent[0:hs, w - ws : w] = imgSmall
+    imgCurrent = cv2.resize(imgCurrent, (1920, 1080))
 
     cv2.imshow("Hand Gesture Recognition", imgCurrent)
     cv2.imshow("Image", img)
